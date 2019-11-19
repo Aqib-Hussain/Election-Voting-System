871ec3618d585dda7a15efe8a0e9829612dd373c
Aqib <Aqib.Hussain.1@protonmail.com>
2019-11-19 20:32:29 +0000

    Updated election class.

--- election.cpp
+++ election.cpp
@@ -4,7 +4,7 @@
 
 #include "election.h"
 #include "vote.h"
-
+#include <sstream>
 
 void election::add_vote(const vote &v){
     votes.push_back(v);
@@ -24,6 +24,27 @@ void election::eliminate(candidate c){
     });
 }
 
-std::vector<std::pair<candidate, int>> ranked_candidates() const{
+std::vector<std::pair<candidate, int>> election::ranked_candidates() const{
+    std::vector<std::pair<candidate,int>> pairs;
+    //loop through all votes
+    for(const vote& vote: votes){
+        //create bool if the candidate and vote match
+        bool counted = false;
+        //for search through 2nd vector pair for voting pairs
+        for(std::pair<candidate,int> &pair: pairs ){
+            if (pair.first ==  vote.first_preference()){
+                pair.second = pair.second+1;
+                counted = true;
+            }
+        }
+        //new candidate gets created and gets 1 vote added to their tally
+        if (!counted){
+            pairs.emplace_back(vote.first_preference(), 1);
+        }
+    }
+    return pairs;
+}
 
+election read_votes(std::istream &in){
+   // return;
 }
