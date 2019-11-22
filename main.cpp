79756974cc7df2541b780cf85e39e02679ec181e
Aqib <Aqib.Hussain.1@protonmail.com>
2019-11-22 17:49:01 +0000

    Updated project to completion.

--- main.cpp
+++ main.cpp
@@ -9,15 +9,9 @@ int main() {
         auto election = read_votes(in);
         bool winnerFound = false;
         int roundCounter = 1;
-        int candidateCounter = 1;
         while (!winnerFound) {
             std::cout << "Round " << roundCounter << ": " << election.vote_count() << " votes" << "\n";
             std::cout << "First preferences: " << "\n";
-//            while (candidateCounter < election.ranked_candidates().size()) {
-//                std::cout << "Candidate " << candidateCounter << ": " << election.ranked_candidates().front().second
-//                          << "\n";
-//                candidateCounter = candidateCounter + 1;
-//            }
 
             for(std::pair<candidate , int> p : election.ranked_candidates()){
                 std::cout << "  Candidate " << p.first << ": " << p.second << "\n";
@@ -35,7 +29,4 @@ int main() {
         }
     }
     return 0;
-    //type out all candidates in for loop
-    //work on formatting
-    //make sure candidate numbers can vary
 }
